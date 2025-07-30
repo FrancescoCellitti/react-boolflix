d58fa6d9952bb81b9a9b1dc5d5f13d372a2262b6
FrancescoCellitti <francescocellitti@virgilio.it>
2025-07-30 16:22:36 +0200

    hover effetc with css

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -13,35 +13,33 @@ const Language = {
 }
 
 export default function CardMovies({ movies }) {
-    const [showInfo, setShowInfo] = useState(false)
+
 
     return (
 
-        <div className="col col-md-y">
-            <div className="card p-1 my-2 border-0 bg-dark text-white" onMouseEnter={() => setShowInfo(true)}
-                onMouseLeave={() => setShowInfo(false)} style={{ cursor: "pointer" }}>
-                {!showInfo ? (
-                    <img
-                        src={`https://image.tmdb.org/t/p/w342${movies.poster_path}`}
-                        className="netflix-card-img"
-                        alt={movies.title}
-                        style={{
-                            height: "400px",
-                        }}
-                    />) : (
-                    <div className="card">
-                        <ul>
-                            <li>{movies.title}</li>
-                            <li>{movies.original_title}</li>
-                            <li><CountryFlag countryCode={Language[movies.original_language]} svg
-                                style={{ width: '1.5em', height: '1.5em' }} /></li>
-                            <li>{[...Array(5)].map((_, i) => (
-                                <i key={i} className={`fa-star ${i < Math.ceil((movies.vote_average) / 2) ? "fa-solid text-warning" : "fa-regular text-warning"}`}></i>
-                            ))}</li>
-                        </ul>
-                    </div>
-                )}
+        <div className="col col-md-3">
+            <div className="card card-img p-1 my-2 border-0 bg-dark text-white" style={{ minHeight: "400px", height: "400px", position: "relative", overflow: "hidden" }}>
+                <img
+                    src={`https://image.tmdb.org/t/p/w342${movies.poster_path}`}
+                    alt={movies.title}
+                    style={{
+                        height: "400px",
+                    }} />
+
+                <div className="card-info card-img p-1 my-2 border-0 text-white">
+                    <ul className="list-unstyled">
+                        <li>{movies.title}</li>
+                        <li>{movies.original_title}</li>
+                        <li><CountryFlag countryCode={Language[movies.original_language]} svg
+                            style={{ width: '1.5em', height: '1.5em' }} /></li>
+                        <li>{[...Array(5)].map((_, i) => (
+                            <i key={i} className={`fa-star ${i < Math.ceil((movies.vote_average) / 2) ? "fa-solid text-warning" : "fa-regular text-warning"}`}></i>
+                        ))}</li>
+                    </ul>
+                </div>
+
             </div>
+
 
         </div>
 
