d7cb7c041927a64da30d614a430aaeff04dc9a8a
FrancescoCellitti <francescocellitti@virgilio.it>
2025-07-30 15:52:10 +0200

    hover con react

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 import CountryFlag from "react-country-flag";
 
 const Language = {
@@ -13,32 +13,37 @@ const Language = {
 }
 
 export default function CardMovies({ movies }) {
+    const [showInfo, setShowInfo] = useState(false)
+
     return (
-       <div className="col col-md-3 ">
-            <div className="card p-1 my-2 border-0">
-                <img
-                    src={`https://image.tmdb.org/t/p/w342${movies.poster_path}`}
-                    className="netflix-card-img"
-                    alt={movies.title}
-                    style={{
-                        height: "400px",                      
-                    }}
-                />
-        
+
+        <div className="col col-md-y">
+            <div className="card p-1 my-2 border-0 bg-dark text-white" onMouseEnter={() => setShowInfo(true)}
+                onMouseLeave={() => setShowInfo(false)} style={{ cursor: "pointer" }}>
+                {!showInfo ? (
+                    <img
+                        src={`https://image.tmdb.org/t/p/w342${movies.poster_path}`}
+                        className="netflix-card-img"
+                        alt={movies.title}
+                        style={{
+                            height: "400px",
+                        }}
+                    />) : (
+                    <div className="card">
+                        <ul>
+                            <li>{movies.title}</li>
+                            <li>{movies.original_title}</li>
+                            <li><CountryFlag countryCode={Language[movies.original_language]} svg
+                                style={{ width: '1.5em', height: '1.5em' }} /></li>
+                            <li>{[...Array(5)].map((_, i) => (
+                                <i key={i} className={`fa-star ${i < Math.ceil((movies.vote_average) / 2) ? "fa-solid text-warning" : "fa-regular text-warning"}`}></i>
+                            ))}</li>
+                        </ul>
+                    </div>
+                )}
             </div>
+
         </div>
+
     )
 }
-
-
-
-
-/*
-                                    <li key={index}>{movie.title}, {movie.original_title}, <CountryFlag countryCode={Language[movie.original_language]} svg
-                                        style={{ width: '1.5em', height: '1.5em' }} />{movie.original_language}
-                                        <img src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt="" />
-                                        vote: {Math.ceil((movie.vote_average) / 2)} /5
-                                        {[...Array(5)].map((_, i) => (
-                                            <i key={i} className={`fa-star ${i < Math.ceil((movie.vote_average) / 2) ? "fa-solid text-warning" : "fa-regular text-warning"}`}></i>
-                                        ))}
-                                    </li>*/
